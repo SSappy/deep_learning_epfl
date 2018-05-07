01111e1a18015a0b262100f5653a7f6df645a647
SSappy <quentin.rebjock@gmail.com>
2018-05-07 16:28:30 +0200

    tried to make rnns function

--- project1/src/rec_models.py
+++ project1/src/rec_models.py
@@ -10,7 +10,7 @@ from nnmodel import NNModel
 class BasicLSTM(NNModel):
 
     def __init__(self, input_size, hidden_size):
-        super(LSTMTagger, self).__init__()
+        super(BasicLSTM, self).__init__()
         self.hidden_size = hidden_size
 
         # The LSTM takes word embeddings as inputs, and outputs hidden states
@@ -30,7 +30,35 @@ class BasicLSTM(NNModel):
                 torch.zeros(2, 16, self.hidden_size))
 
     def forward(self, x):
+        print('x ', x.shape)
         lstm_out, self.hidden = self.lstm(x, self.hidden)
+        print('lstm out ', lstm_out.shape)
         tag_space = self.hidden2tag(x.view(len(x), -1))
         tag_scores = F.log_softmax(tag_space, dim=1)
         return tag_scores
+
+
+class RNN(NNModel):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+        # self.dropout = nn.Dropout(0.4)
+
+    def forward(self, x):
+        # Set initial states
+        print('x ', x.shape)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+
+        print('h0 ', h0.shape)
+
+        # Forward propagate RNN
+        out, _ = self.lstm(x, (h0, c0))
+
+        # Decode hidden state of last time step
+        print('out[:, -1, :] ', out[:, -1, :].shape)
+        out = self.fc(out[:, -1, :])
+        return out

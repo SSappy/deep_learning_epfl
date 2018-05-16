fab9d6c0c108f20b2ce25dbd4cfde8732c20f8b3
Armand <armand.boschin@gmail.com>
2018-05-16 21:48:17 +0200

    finalized test.py file adding training time and test scores

--- project2/src/test.py
+++ project2/src/test.py
@@ -3,6 +3,7 @@ from linear import Linear
 from activations import ReLU, Tanh
 from losses import MSE
 from utils import build_data
+from time import time
 
 def build_model():
     model = Sequential(MSE(), input_size=2)
@@ -16,15 +17,24 @@ def build_model():
     return model
 
 
-def build_and_train_model(n=1000, epochs=50, step_size=0.01):
-    print('\nGenerating training and validation sets of size {} each.'.format(n))
+def build_and_train_model(n=1000, epochs=70, step_size=0.01):
+    print('\nGenerating training, validation and test sets of size {} each.'.format(n))
     x_train, y_train = build_data(n)
     x_validation, y_validation = build_data(n)
+    x_test, y_test = build_data(n)
     print('Building the model\n')
     model = build_model()
     model.summary()
     print('\nTraining the model on {} epochs'.format(epochs))
+    t = time()
     model.fit(x_train, y_train, x_validation, y_validation, epochs=epochs, step_size=step_size)
+    t = int(time()- t)
+    print('\nTraining time : {0:0.0f} seconds ({1:0.3f} seconds per epoch)'.format(t, t/epochs))
+    print('\nTesting the trained model :')
+    te_predictions, te_loss = model.predict(x_test, y_test)
+    te_accuracy = (te_predictions == y_test).sum() / y_test.shape[1] / te_predictions.shape[0]
+    print('Test loss : {}'.format(te_loss.mean()))
+    print('Test accuracy : {}'.format(te_accuracy))
 
 if __name__ == '__main__':
     try:
@@ -36,6 +46,7 @@ if __name__ == '__main__':
                                 message="other is not broadcastable to self, but they have the same number of elements.  "
                                         "Falling back to deprecated pointwise behavior.")
         build_and_train_model()
+
 
     except AssertionError:
         print('Beware that the code was developed under torch 0.3.1.')
